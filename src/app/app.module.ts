039fe2242c8b80d90c4a1e25b1261bb82dc090e2
vantamin <vantablack.min@gmail.com>
2022-09-08 13:16:54 +0900

    Create hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent, SafeHtmlPipe],
-  imports: [BrowserModule],
+  declarations: [AppComponent, SafeHtmlPipe, HeroesComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
